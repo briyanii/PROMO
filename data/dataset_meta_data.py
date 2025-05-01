b774f0e209575f9198ed998916f9cf2414939742
briyanii <bri.n.fung@gmail.com>
2025-05-01 16:01:03 +0800

    Update dataset_meta_data.py

--- data/dataset_meta_data.py
+++ data/dataset_meta_data.py
@@ -6,16 +6,24 @@ MovieLens100k_mata_data = {"user_num": 943,
                            "item_num": 1682,
                            "user_feature": {
                                "dim": 23,
-                               "nume_feat_idx": [0], # 0 age:0~1
-                               "cate_id_feat_idx": [(1,2)], # 1 gender:0-1
-                               "cate_one_hot_feat_idx": [(2, 21)] # 2-21 occupation: one hot
+                               "nume_feat_idx": [
+                                    0 # age
+                                ],
+                               "cate_id_feat_idx": [
+                                    (1,2) # gender
+                                ],
+                               "cate_one_hot_feat_idx": [
+                                    (2, 21) # occupation
+                                ] # 2-21 occupation: one hot
                                # 类别特征：id类是(idx, cate_num)，one hot类是(start_idx, end_idx)
                             },
                             "item_feature":{
                                 "dim": 18,
                                 "nume_feat_idx": [],
                                 "cate_id_feat_idx": [],
-                                "cate_one_hot_feat_idx": [(0, 17)] # 0-17 genre: one hot / multi hot
+                                "cate_one_hot_feat_idx": [
+                                    (0, 17) # genre
+                                ] # 0-17 genre: one hot / multi hot
                             }
                            }
 meta_data['MovieLens100k'] = MovieLens100k_mata_data
@@ -24,16 +32,24 @@ MovieLens1m_mata_data = {"user_num": 6040,
                          "item_num": 3952,
                          "user_feature": {
                                "dim": 23,
-                               "nume_feat_idx": [0], # 0 age:0~1
-                               "cate_id_feat_idx": [(1, 2)], # 1 gender:0-1
-                               "cate_one_hot_feat_idx": [(2, 21)] # 2-21 occupation: one hot / multi hot
-                               # 类别特征：id类是(idx, cate_num)，one hot类是(start_idx, end_idx)
+                               "nume_feat_idx": [
+                                    0
+                                ], # 0 age:0~1
+                               "cate_id_feat_idx": [
+                                    (1, 2) # gender
+                                ],
+                               "cate_one_hot_feat_idx": [
+                                    (2, 21) # occupation
+                                ]
+                               # 类别特征：id类是(idx, cate_num)，one hot类是(start_idx, end_idx
                          },
                          "item_feature":{
                             "dim": 18,
                             "nume_feat_idx": [],
                             "cate_id_feat_idx": [],
-                            "cate_one_hot_feat_idx": [(0, 17)] # 0-17 genre: one hot
+                            "cate_one_hot_feat_idx": [
+                                (0, 17) # genre
+                            ]
                          }
                         }
 meta_data['MovieLens1m'] = MovieLens1m_mata_data
@@ -43,23 +59,98 @@ KuaiRand_mata_data = {"user_num": 27285,
                          "user_feature": {
                                "dim": 25,
                                "nume_feat_idx": [],
-                               "cate_id_feat_idx": [(0, 4), (1, 2), (2, 2), (3, 8), (4, 7),
-                                                    (5, 7), (6, 7), (7, 2), (8, 7), (9, 50),
-                                                    (10, 1471), (11, 15), (12, 34), (13, 3),
-                                                    (14, 118), (15, 454), (16, 7), (17, 5), (18, 5), (19, 2),
-                                                    (20, 2), (21, 2), (22, 2), (23, 2), (24, 2)], #
+                               "cate_id_feat_idx": [
+                                    (0, 4), # user active degree
+                                            # is_lowactive_period
+                                    (1, 2), # is_livestreamer
+                                    (2, 2), # is_video_author
+                                    (3, 8), # user_follow_num_range
+                                    (4, 7), # fan_user_num_range
+                                    (5, 7), # friend_user_num_range
+                                    (6, 7), # register_days_range
+                                    (7, 2), # onehot_feat0
+                                    (8, 7), # onehot_feat1
+                                    (9, 50)
+                                    (10, 1471),
+                                    (11, 15),
+                                    (12, 34),
+                                    (13, 3)
+                                    (14, 118),
+                                    (15, 454),
+                                    (16, 7),
+                                    (17, 5),
+                                    (18, 5),
+                                    (19, 2)
+                                    (20, 2),
+                                    (21, 2),
+                                    (22, 2),
+                                    (23, 2),
+                                    (24, 2) # onehot_feat17
+                                ], #
                                "cate_one_hot_feat_idx": []
                                # 类别特征：id类是(idx, cate_num)，one hot类是(start_idx, end_idx)
                          },
                          "item_feature":{
                             "dim": 55,
-                            "nume_feat_idx": [4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-                                              14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-                                              24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
-                                              34, 35, 36, 37, 38, 39, 40, 41, 42, 43,
-                                              44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
-                                              54],
-                            "cate_id_feat_idx": [(0,2), (1,4), (2,6), (3,69)],
+                            "nume_feat_idx": [ # all statistic features excluding video id
+                                4, # counts
+                                5,
+                                6,
+                                7,
+                                8,
+                                9,
+                                10,
+                                11,
+                                12,
+                                13,
+                                14,
+                                15,
+                                16,
+                                17,
+                                18,
+                                19,
+                                20,
+                                21,
+                                22,
+                                23,
+                                24,
+                                25,
+                                26,
+                                27,
+                                28,
+                                29,
+                                30,
+                                31,
+                                32,
+                                33,
+                                34,
+                                35,
+                                36,
+                                37,
+                                38,
+                                39,
+                                40,
+                                41,
+                                42,
+                                43,
+                                44,
+                                45,
+                                46,
+                                47,
+                                48,
+                                49,
+                                50,
+                                51,
+                                52,
+                                53,
+                                54
+                            ],
+                            "cate_id_feat_idx": [
+                                    (0,2), # video_type
+                                    (1,4), # video_duration (range)
+                                    (2,6), # music type
+                                    (3,69) # tag
+                                ],
                             "cate_one_hot_feat_idx": []
                          }
                         }
